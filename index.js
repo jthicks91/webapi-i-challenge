31a4cad08658d6ec2bbf16d934102130779df536
jthicks91 <jordanthicks91@gmail.com>
2019-05-08 18:57:52 -0700

    finished writing all endpoints to perform queries. working on endpoint specifications now.

--- index.js
+++ index.js
@@ -39,6 +39,56 @@ server.post("/users", (req, res) => {
     });
 });
 
+// Removes the user with the specified id and returns the deleted user.
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  hubs
+    .remove(id)
+    .then(removedUser => {
+      if (removedUser) {
+        res.json(removedUser);
+      } else {
+        res.status(404).json({ err: "incorrect id" });
+      }
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ err: message });
+    });
+});
+
+// Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
+server.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  hubs
+    .update(id, changes)
+    .then(updatedUser => {
+      if (updatedUser) {
+        res.json(updatedUser);
+      } else {
+        res.status(404).json({ err: "incorrect id" });
+      }
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ err: message });
+    });
+});
+
+//Returns the user object with the specified id.
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  hubs
+    .findById(id)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ err: message });
+    });
+});
+
 server.listen(3000, () => {
   console.log("Listening on port 3000");
 });
